d28cc888bd97e2ea4fd96820d17358ebdc55267d
guilhermeaeme <guilhermeaeme@gmail.com>
2019-03-30 14:19:32 -0300

    Iteration 1

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import contacts from './contacts.json'
+
 class App extends Component {
+  state = {
+    filteredContacts: [...contacts].slice(0, 5)
+  }
+
   render() {
+    console.log(this.state.filteredContacts)
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>IronContacts</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.filteredContacts.map((contact, index) => {
+              return (
+                <tr key={index}>
+                  <td><img src={contact.pictureUrl} alt={contact.name} /></td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity.toFixed(2)}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
